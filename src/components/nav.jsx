cd47ec9cd29a6e7cf1270651eef3d3e2fa4e8ac3
Adrian Grimm <usmcamgrimm@gmail.com>
2020-05-19 06:18:30 -0400

    Use Link to start nav

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,5 +1,24 @@
-import React from 'react'
+import React from "react"
+import { Link } from "gatsby"
 
-export default function Nav () {
-  return <h3>This will be a navbar</h3>
+export default function Nav() {
+  return (
+    <ul>
+      <li>
+        <Link to="/index/">Home</Link>
+      </li>
+      <li>
+        <Link to="/music/">Music</Link>
+      </li>
+      <li>
+        <Link to="/about/">About</Link>
+      </li>
+      <li>
+        <Link to="/contact/">Contact</Link>
+      </li>
+      <li>
+        <Link to="/disclaimer/">Disclaimer</Link>
+      </li>
+    </ul>
+  )
 }
