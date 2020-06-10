1232719882b49aeace201b04fea360345810ad1f
Adrian Grimm <usmcamgrimm@gmail.com>
2020-06-10 17:26:46 -0400

    clean up css imports

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import headerStyle from "./header.module.css"
+import "./header.module.css"
 
 export default function Header(props) {
   return <h1>{props.headerText}</h1>
