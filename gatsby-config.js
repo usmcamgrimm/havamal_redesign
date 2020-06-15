8736b4f756bcc638eb82f83139be74c7b6958da9
Adrian Grimm <usmcamgrimm@gmail.com>
2020-06-15 06:41:31 -0400

    Revert "Update the config"
    This reverts commit d2dac5f8c2f42f48fb5e21b064a0ce4373174e63.

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,33 +4,30 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
-;(module.exports = {
+module.exports = {
   /* Your site config here */
-  plugins: []
-}),
-  {
-    resolve: `gatsby-plugin-prefetch-google-fonts`,
-    options: {
-      fonts: [
-        {
-          family: `Mirza`,
-          variants: [`400`]
-        },
-        {
-          family: `Roboto`,
-          variants: [`300`, `300i`, `400`, `400i`, `500`, `800`]
-        }
-      ]
-    }
-  },
-  {
-    resolve: 'gatsby-source-graphql',
-    options: {
-      typeName: 'Fauna',
-      fieldName: 'fauna',
-      url: 'https://graphql.fauna.com/graphql',
-      headers: {
-        Authorization: 'Bearer <FAUNA_KEY>'
-      }
-    }
+  plugins: [],
+}, {
+  resolve: `gatsby-plugin-prefetch-google-fonts`,
+  options: {
+    fonts: [{
+        family: `Mirza`,
+        variants: [`400`],
+      },
+      {
+        family: `Roboto`,
+        variants: [`300`, `300i`, `400`, `400i`, `500`, `800`],
+      },
+    ],
   }
+}, {
+  resolve: "gatsby-source-graphql",
+  options: {
+    typeName: "Fauna",
+    fieldName: "fauna",
+    url: "https://graphql.fauna.com/graphql",
+    headers: {
+      Authorization: "Bearer <SERVER KEY>",
+    },
+  },
+}
