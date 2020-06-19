edfeb7416733113086d1af04f4ad1f617b09a328
Adrian Grimm <usmcamgrimm@gmail.com>
2020-06-19 18:36:15 -0400

    Getting started on a new Navbar
    For now, I've got styled-components installed
    Added a Logo file with Gatsby-image and graphql

---
+++ src/components/Navbar/Logo.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import Img from "gatsby-image"
+import { useStaticQuery, graphql } from "gatsby"
+
+const Logo = () => {
+  const data = useStatucQuery(graphql`
+        query {
+            file(name: { eq: 'axes' }, extension: { eq: 'png' }) {
+                childImageSharp {
+                    fluid(maxWidth: 108, pngQuality: 80) {
+                        ...GatsbyImageSharpFluid
+                    }
+                }
+            }
+        }
+    `)
+  return <Img fluid={data.file.childImageSharp.fluid} alt="logo" />
+}
+
+export default Logo
