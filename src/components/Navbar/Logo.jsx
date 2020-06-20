2293c5c23eaa79ec685b12a8d5c2a912a2b903de
Adrian Grimm <usmcamgrimm@gmail.com>
2020-06-20 12:43:42 -0400

    Add a wrapper to the logo

--- src/components/Navbar/Logo.jsx
+++ src/components/Navbar/Logo.jsx
@@ -1,20 +1,34 @@
 import React from "react"
+import styled from "styled-components"
 import Img from "gatsby-image"
-import { useStaticQuery, graphql } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
+
+const LogoWrap = styled.div`
+  margin: auto 0;
+  flex: 0 1 36px;
+
+  @media (max-width: 768px) {
+    flex: 0 1 25px;
+  }
+`
 
 const Logo = () => {
   const data = useStaticQuery(graphql`
     query {
       file(name: { eq: "axes" }, extension: { eq: "png" }) {
         childImageSharp {
-          fluid(maxWidth: 108, pngQuality: 80) {
+          fluid(maxWidth: 50, pngQuality: 80) {
             ...GatsbyImageSharpFluid
           }
         }
       }
     }
   `)
-  return <Img fluid={data.file.childImageSharp.fluid} alt="logo" />
+  return (
+    <LogoWrap as={Link} to="/">
+      <Img fluid={data.file.childImageSharp.fluid} alt="logo" />
+    </LogoWrap>
+  )
 }
 
 export default Logo
