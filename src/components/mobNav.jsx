7f5d7a1a47ca77432df4340077ff1dc2ec869a27
Adrian Grimm <usmcamgrimm@gmail.com>
2020-06-21 15:17:19 -0400

    Re-adding styled componentes to add to this nav toggle
    warn ESLintError:
    D:\GitHub\havamal\src\components\mobNav.jsx
      31:7  warning  Visible, non-interactive elements with click handlers must have at least one keyboard listener
    jsx-a11y/click-events-have-key-events
      31:7  warning  Non-interactive elements should not be assigned mouse or keyboard event listeners
    jsx-a11y/no-noninteractive-element-interactions
    
    I need a fucking break

--- src/components/mobNav.jsx
+++ src/components/mobNav.jsx
@@ -1,25 +1,55 @@
-import React from "react"
+import React, { useState } from "react"
 import NavLinks from "./NavLinks.jsx"
 import mobStyles from "./nav.module.css"
 import axes from "../images/axes.png"
+import styled from "styled-components"
 
-export default function Nav() {
+const NavOverlay = styled.div`
+  display: flex;
+  height: 100%;
+  justify-content: flex-end;
+  align-items: center;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    position: fixed;
+    width: 100%;
+    justify-content: flex-start;
+    padding-top: 10vh;
+    background-color: #fff;
+    transition: all 0.3s ease-in;
+    top: 8vh;
+    left: ${props => (props.open ? "-100%" : "0")};
+  }
+`
+
+const MobNav = () => {
+  const [navOpen, setNavOpen] = useState(false)
+
   return (
-    <>
-      <div id="mobNav" className="navOverlay">
-        {/* <button>&times;</button> */}
-        <NavLinks />
-      </div>
-
-      <div className="mobFloat">
-        <img
-          src={axes}
-          alt="crossed axes button"
-          className={mobStyles.axeBtn}
-          width="60"
-          height="auto"
-        />
-      </div>
-    </>
+    <div className="mobFloat">
+      <img
+        src={axes}
+        alt="crossed axes button"
+        className={mobStyles.axeBtn}
+        width="60"
+        height="auto"
+        navopen={navOpen}
+        onClick={() => setNavOpen(!navOpen)}
+      >
+        {navOpen}
+      </img>
+      {navOpen ? (
+        <NavOverlay>
+          <NavLinks />
+        </NavOverlay>
+      ) : (
+        <NavOverlay open>
+          <NavLinks />
+        </NavOverlay>
+      )}
+    </div>
   )
 }
+
+export default MobNav
