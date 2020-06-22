26889108bd7c55c5ad1aae8646c0fc5a04b8a09d
Adrian Grimm <usmcamgrimm@gmail.com>
2020-06-22 18:59:44 -0400

    Scrap this to start over again

--- src/components/header.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import "./header.module.css"
-
-export default function Header(props) {
-  return <h2>{props.headerText}</h2>
-}
--- src/components/nav.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import NavLinks from "./NavLinks"
-import "./nav.module.css"
-
-export default function Nav() {
-  return <NavLinks />
-}
