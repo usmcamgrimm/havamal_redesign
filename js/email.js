265c3ba2fd52f0ad013584003e46c32a2720758b
Adrian Grimm <usmcamgrimm@gmail.com>
2020-07-16 13:01:49 -0400

    Add email script

--- js/email.js
+++ js/email.js
@@ -1,1 +1,16 @@
 import emailjs from 'emailjs-com'
+
+const form = document.getElementById('email-form-react')
+
+export default function Email() {
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs.sendForm('process.env.service_id', 'process.env.template_id', 'process.env.user_id')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    }
+}
