bb1d57a95667caa318581810905755804405a5d9
Adrian Grimm <usmcamgrimm@gmail.com>
2020-07-17 06:41:29 -0400

    Try working on the email script

--- js/email.js
+++ js/email.js
@@ -6,11 +6,11 @@ export default function Email() {
     function sendEmail(e) {
         e.preventDefault();
 
-        emailjs.sendForm('process.env.service_id', 'process.env.template_id', 'process.env.user_id')
+        emailjs.sendForm('process.env.service_id', 'process.env.template_id', e.target, 'process.env.user_id')
             .then((result) => {
-                console.log(result.text);
+                console.log('Success!', result.status, result.text);
             }, (error) => {
-                console.log(error.text);
+                console.log('Send failed...', error);
             });
     }
 }
