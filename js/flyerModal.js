e2032ebe9dba921ab3215071100bb91a60ce9f2d
Adrian Grimm <usmcamgrimm@gmail.com>
2020-08-02 14:25:35 -0400

    Add a modal to display show flyers

---
+++ js/flyerModal.js
@@ -0,0 +1,17 @@
+let flyerModal = document.getElementById("flyerModal");
+
+let img = document.getElementById("flyer");
+let modalImg = document.getElementById("flyerImg");
+let captionText = document.getElementById("caption");
+
+img.onclick = function () {
+    flyerModal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.lastChild;
+}
+
+let span = document.getElementsByClassName("close")[0];
+
+span.onclick = function () {
+    modalImg.style.display = "none";
+}
